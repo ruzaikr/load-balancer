7b265d90c2f132950b2759b0cb8155b2a5731bad
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-04-20 22:56:40 +0530

    Add retry logic

--- src/main/java/org/coda/resources/RoundRobin.java
+++ src/main/java/org/coda/resources/RoundRobin.java
@@ -1,19 +1,19 @@
 package org.coda.resources;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.client.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
+import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("/{any:.*}")
 public class RoundRobin {
@@ -25,35 +25,58 @@ public class RoundRobin {
     );
 
     private static final AtomicInteger COUNTER = new AtomicInteger(0);
+    private static final int MAX_RETRIES = 3;
+    private static final int TIMEOUT_MS = 2_000; // 2 seconds
 
     @Context UriInfo uriInfo;
 
     private static final Client CLIENT = ClientBuilder.newBuilder()
             .register(JacksonFeature.class)
+            .property(ClientProperties.CONNECT_TIMEOUT, TIMEOUT_MS)
+            .property(ClientProperties.READ_TIMEOUT, TIMEOUT_MS)
             .build();
+
+    private static final Logger logger = Logger.getLogger(RoundRobin.class.getName());
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response proxy(JsonNode payload) {
-        int nextIndex = Math.abs(COUNTER.getAndIncrement() % BACKENDS.size());
 
-        WebTarget target = CLIENT
-                .target(BACKENDS.get(nextIndex))
-                .path(uriInfo.getPath());
+        for (int attemptNum = 1; attemptNum <= MAX_RETRIES; attemptNum++) {
+            int nextRRIndex = Math.abs(COUNTER.getAndIncrement() % BACKENDS.size());
 
-        Invocation invocation = target
-                .request(MediaType.APPLICATION_JSON)
-                .buildPost(Entity.json(payload));
+            WebTarget target = CLIENT
+                    .target(BACKENDS.get(nextRRIndex))
+                    .path(uriInfo.getPath());
 
-        try (Response backendResp = invocation.invoke()) {
+            Invocation invocation = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .buildPost(Entity.json(payload));
 
-            JsonNode responseBody = backendResp.readEntity(JsonNode.class);
+            try (Response backendResp = invocation.invoke()) {
+                int status = backendResp.getStatus();
+                if (status >= 500 && status < 600) {
+                    continue;
+                }
 
-            return Response.status(backendResp.getStatus())
-                    .entity(responseBody)
-                    .build();
+                JsonNode responseBody = backendResp.readEntity(JsonNode.class);
+                return Response
+                        .status(backendResp.getStatus())
+                        .entity(responseBody)
+                        .build();
+            } catch (ProcessingException exception) {
+                logger.log(Level.WARNING,
+                        "Attempt {0}/{1}: Backend {2} unavailable. Reason: {3}",
+                        new Object[]{attemptNum, MAX_RETRIES, target.getUri(), exception.getMessage()});
+            }
         }
+
+        return Response
+                .status(Response.Status.SERVICE_UNAVAILABLE)
+                .entity("{\"error\":\"Service unavailable.\"}")
+                .build();
+
     }
 
 
