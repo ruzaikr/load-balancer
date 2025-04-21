27eae5aae51e1e149e9f6ac231ad40d3f81bae34
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-04-21 23:10:00 +0530

    Implement periodic health checker

--- src/main/java/org/coda/service/LoadBalancerService.java
+++ src/main/java/org/coda/service/LoadBalancerService.java
@@ -7,9 +7,14 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,6 +24,7 @@ import org.coda.config.AppConfig;
 public class LoadBalancerService {
 
   private final List<String> backends;
+  private final ConcurrentMap<String, Boolean> healthyMap;
   private final Client client;
   private final int maxRetries;
   private final AtomicInteger counter = new AtomicInteger();
@@ -29,11 +35,42 @@ public class LoadBalancerService {
     this.backends = appConfig.getBackends();
     this.client = ClientFactory.create(appConfig);
     this.maxRetries = appConfig.getMaxRetries();
+
+    this.healthyMap = new ConcurrentHashMap<>();
+    backends.forEach(backend -> healthyMap.put(backend, true));
+
+    Executors.newSingleThreadScheduledExecutor(runnable -> {
+      Thread thread = new Thread(runnable, "health-checker");
+      thread.setDaemon(true);
+      return thread;
+    }).scheduleAtFixedRate(this::updateHealthStatus, 0, 10, TimeUnit.SECONDS);
+  }
+
+  private void updateHealthStatus() {
+    for (String backend : backends) {
+      boolean isHealthy;
+      try (Response response = client
+          .target(backend)
+          .path("/health")
+          .request()
+          .get()) {
+        isHealthy = (response.getStatus() == Status.OK.getStatusCode());
+      } catch (Exception exception) {
+        isHealthy = false;
+      }
+      healthyMap.put(backend, isHealthy);
+    }
   }
 
   public Response proxy(JsonNode payload, UriInfo uriInfo) {
     for (int attemptNum = 1; attemptNum <= maxRetries; attemptNum++) {
       int nextRoundRobinIndex = getNextRoundRobinIndexAndIncrementCounter();
+      String backend = backends.get(nextRoundRobinIndex);
+
+      if (!healthyMap.get(backend)) {
+        continue;
+      }
+
       URI targetUri = URI.create(backends.get(nextRoundRobinIndex))
           .resolve(uriInfo.getPath());
 
@@ -53,6 +90,7 @@ public class LoadBalancerService {
             .entity(backendResponseBody)
             .build();
       } catch (ProcessingException processingException) {
+        healthyMap.put(backend, false);
         logger.log(Level.WARNING,
             "Attempt {0}/{1}: Backend {2} unavailable. Reason: {3}",
             new Object[]{attemptNum, maxRetries, targetUri, processingException.getMessage()});
